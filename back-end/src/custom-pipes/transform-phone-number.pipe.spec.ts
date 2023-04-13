836bc8fa19764b8130ef7c26eaf6626472727a1c
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-13 22:32:24 +0000

    Added a custom-pipes folder in which we will put our custom pipes, and we added our first custom pipe which is a transformation pipe that will transform the phone number to the format +212 xxx xxx xxx, to insure that we are storing the phone numbers in the same format in the DB.

---
+++ back-end/src/custom-pipes/transform-phone-number.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { TransformPhoneNumberPipe } from './transform-phone-number.pipe';
+
+describe('TransformPhoneNumberPipe', () => {
+  it('should be defined', () => {
+    expect(new TransformPhoneNumberPipe()).toBeDefined();
+  });
+});
