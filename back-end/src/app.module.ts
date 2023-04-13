13a7a36d2a5d3c2d2d843ad0fe3e6a43292fd1f4
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-13 22:58:43 +0000

    In the app module we added some intersting stuff, first we registered the TypeORM module with the neccessary configuration to connect to our Postgres RDBMS, second we registred our ConfigModule that will help us to parse the env files and merge them with the node process envs, third we registred our user module to handler the user business logic. Finally, we registred the class serializer interceptor that will help use to sinitize our response data before returing it to the client.

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './users/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    /**
+     * Configure TypeORM for the Postgres RDBMS with the config service,
+     * we used the Async configuration to inject the ConfigService.
+     */
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        migrations: ['dist/migrations/*.js'],
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
