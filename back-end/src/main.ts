37e0229cd423f39328247d04d3ad09a89d4d7f12
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-13 23:04:30 +0000

    In the main file we added two important things, the first one a global validation pipe for input validations, the second one a cookie parser middleware to parse cookies.

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Enable global validation and transformation pipe,
+   * this will be applied to all routes, and will validate
+   * and transform the body, query, and params of the request,
+   * and it will strip non-whitelisted properties from the body.
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  /**
+   * Enable cookie parser middleware, it will parse the cookies
+   * and set them to the request object.
+   */
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
