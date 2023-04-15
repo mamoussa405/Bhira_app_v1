b7c5e7d1586cb75c35ea26e8ba37659d3889edb3
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-15 15:50:29 +0000

    Changed the isAdmin and confirmedByAdmin columns to not be false by default, that's because we want to control them by our entities.

--- back-end/src/migrations/1681279367699-UserEntity.ts
+++ back-end/src/migrations/1681279367699-UserEntity.ts
@@ -9,8 +9,8 @@ export class UserEntity1681279367699 implements MigrationInterface {
                 "phoneNumber" VARCHAR(255) UNIQUE NOT NULL,
                 address VARCHAR(255) NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                "isAdmin" BOOLEAN NOT NULL DEFAULT false,
-                "confirmedByAdmin" BOOLEAN NOT NULL DEFAULT false
+                "isAdmin" BOOLEAN NOT NULL,
+                "confirmedByAdmin" BOOLEAN NOT NULL
             )
         `);
   }
