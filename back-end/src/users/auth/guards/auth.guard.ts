27c0c2a0793da6f8b4967b18bedcfa52f3f39cda
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-15 16:55:36 +0000

    Added the logic bypass the guard when we have a public route. PS: public route is route handler marked with @Public decorator.

--- back-end/src/users/auth/guards/auth.guard.ts
+++ back-end/src/users/auth/guards/auth.guard.ts
@@ -5,7 +5,9 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { PublicEnum } from 'src/enums/common.enum';
 
 /**
  * AuthGuard is a guard that will be used to protect routes,
@@ -19,9 +21,22 @@ export class AuthGuard implements CanActivate {
   constructor(
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    /**
+     * We use the reflector to get the @Public() decorator that is
+     * used to add the IS_PUBLIC_ROUTE metadata to the handler,
+     * if it exists, we return true, if it doesn't, we continue,
+     * this will allow us to use the @Public() decorator on routes,
+     * that we don't want to be protected by the AuthGuard.
+     */
+    const isPlublic = this.reflector.get<boolean>(
+      PublicEnum.IS_PUBLIC_ROUTE,
+      context.getHandler(),
+    );
+    if (isPlublic) return true;
     /**
      * First, we get the request object from the context
      * Second, we get the access_token from the request cookies
