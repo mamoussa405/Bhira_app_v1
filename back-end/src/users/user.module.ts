54606fe8dc1afe9713d440975a8c9ce5d364e7a2
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-15 17:00:48 +0000

    Imported the admin module.

--- back-end/src/users/user.module.ts
+++ back-end/src/users/user.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AdminModule],
 })
 export class UserModule {}
