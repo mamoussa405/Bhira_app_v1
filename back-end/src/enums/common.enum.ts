2bc0cde2e3ddcdee035d577fbe0e89423d1676ff
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-15 15:48:00 +0000

    Added enums folder wich is used to store the common enums, by common we mean tha those enums will be used in diffrent places.

---
+++ back-end/src/enums/common.enum.ts
@@ -0,0 +1,7 @@
+/**
+ * Common enum, used for common values, by common values
+ * we mean values that are used in multiple places.
+ */
+export enum PublicEnum {
+  IS_PUBLIC_ROUTE = 'isPublicRoute',
+}
