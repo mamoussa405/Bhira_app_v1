fbac2e0d8f392b1373113130c45770641fdf6679
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-16 17:59:53 +0000

    Imported the OrderModule.

--- back-end/src/users/user.module.ts
+++ back-end/src/users/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { OrderModule } from 'src/orders/order.module';
 
 @Module({
-  imports: [AuthModule, AdminModule],
+  imports: [AuthModule, AdminModule, OrderModule],
 })
 export class UserModule {}
