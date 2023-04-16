a09d2f82413144df5ff013cfe9022daf8ad9525a
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-16 17:47:46 +0000

    Changed the way we create tables from SQL core queries to a more sophisticated method using the Table class.

--- back-end/src/migrations/1681279367699-UserEntity.ts
+++ back-end/src/migrations/1681279367699-UserEntity.ts
@@ -1,21 +1,56 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class UserEntity1681279367699 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            CREATE TABLE user_entity (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                "phoneNumber" VARCHAR(255) UNIQUE NOT NULL,
-                address VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                "isAdmin" BOOLEAN NOT NULL,
-                "confirmedByAdmin" BOOLEAN NOT NULL
-            )
-        `);
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_entity',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'phoneNumber',
+            type: 'varchar',
+            length: '255',
+            isUnique: true,
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'confirmedByAdmin',
+            type: 'boolean',
+            // !change this to false before production
+            default: true,
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE user_entity`);
+    await queryRunner.dropTable('user_entity');
   }
 }
--- back-end/src/migrations/1681481631543-ProductEntity.ts
+++ back-end/src/migrations/1681481631543-ProductEntity.ts
@@ -1,22 +1,63 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class ProductEntity1681481631543 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // !important add: "imageURL" VARCHAR(255) NOT NULL
-    await queryRunner.query(`
-            CREATE TABLE product_entity (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                category VARCHAR(255) NOT NULL,
-                price NUMERIC NOT NULL,
-                description VARCHAR(255) NOT NULL,
-                stock NUMERIC NOT NULL,
-                "isNormalProduct" BOOLEAN NOT NULL
-                )
-          `);
+    await queryRunner.createTable(
+      new Table({
+        name: 'product_entity',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'category',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'price',
+            type: 'numeric',
+            precision: 10,
+            scale: 2,
+            default: 0.0,
+          },
+          {
+            name: 'description',
+            type: 'text',
+          },
+          {
+            name: 'stock',
+            type: 'numeric',
+            precision: 10,
+            scale: 2,
+            default: 0.0,
+          },
+          {
+            name: 'isNormalProduct',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'imagesURL',
+            type: 'varchar',
+            length: '500',
+            isArray: true,
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE product_entity`);
+    await queryRunner.dropTable('product_entity');
   }
 }
