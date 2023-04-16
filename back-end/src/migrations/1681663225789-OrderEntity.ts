97c12e0c5d2d5f23dcc1d7942b5d9b57ba956c7f
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-16 17:50:35 +0000

    Added the migration for the order entity.

---
+++ back-end/src/migrations/1681663225789-OrderEntity.ts
@@ -0,0 +1,84 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class OrderEntity1681663225789 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'order_entity',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'orderTime',
+            type: 'timestamp',
+          },
+          {
+            name: 'quantity',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+            default: 0.0,
+          },
+          {
+            name: 'totalPrice',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+            default: 0.0,
+          },
+          {
+            name: 'isAddedToCart',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'buyConfirmedByUser',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'buyConfirmedByAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'buyCanceledByAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'userId',
+            type: 'int',
+          },
+          {
+            name: 'productId',
+            type: 'int',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['userId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'user_entity',
+            onDelete: 'CASCADE',
+          },
+          {
+            columnNames: ['productId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'product_entity',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('order_entity');
+  }
+}
