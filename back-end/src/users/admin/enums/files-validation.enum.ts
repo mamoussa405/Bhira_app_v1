c7b9a0cce5e031431c3f2162296460b1928d1172
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-17 21:04:12 +0000

    We added the enums folder in the admin folder in which we will store our enums that will be used the folder admin scope.

---
+++ back-end/src/users/admin/enums/files-validation.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * This enum is used to let the validation pipe knows
+ * which files to validate, in this case the product images
+ * or the story files.
+ */
+export enum FilesValidation {
+  PRODUCT = 'product',
+  STORY = 'story',
+}
