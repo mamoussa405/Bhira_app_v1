a34b9846b1d4e5d5a2c567a248d2b58f12ab7d0f
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-17 21:01:44 +0000

    In the story service we added to logic to upload the story image and the story video into cloudinary.

--- back-end/src/stories/story.service.ts
+++ back-end/src/stories/story.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { StoryEntity } from './entities/story.entity';
 import { Repository } from 'typeorm';
 import { CreateStoryDto } from 'src/users/admin/dto/create-story.dto';
+import { IFiles } from 'src/types/files.type';
+import { CloudinaryService } from 'nestjs-cloudinary';
 
 /**
  * Service for story related operations.
@@ -13,6 +15,7 @@ export class StoryService {
   constructor(
     @InjectRepository(StoryEntity)
     private readonly storyRepository: Repository<StoryEntity>,
+    private readonly cloundinaryService: CloudinaryService,
   ) {}
 
   /**
@@ -20,12 +23,43 @@ export class StoryService {
    * @param {CreateStoryDto} story - The story to create.
    * @returns {Promise<StoryEntity>} The created story.
    */
-  async createStory(story: CreateStoryDto): Promise<StoryEntity> {
+  async createStory(
+    story: CreateStoryDto,
+    files: IFiles,
+  ): Promise<StoryEntity> {
     try {
       const storyEntity = this.storyRepository.create(story);
+      storyEntity.imageURL = await this.uploadImage(files.image[0]);
+      storyEntity.videoURL = await this.uploadVideo(files.video[0]);
       return await this.storyRepository.save(storyEntity);
     } catch (error) {
       throw new InternalServerErrorException('Error creating story');
     }
+  }
+
+  /**
+   * Upload image to cloudinary and return the url.
+   * @param {Express.Multer.File} image - The image to upload.
+   * @returns {Promise<string>} The url of the uploaded image.
+   */
+  private async uploadImage(image: Express.Multer.File): Promise<string> {
+    const result = await this.cloundinaryService.uploadFile(image, {
+      public_id: image.originalname.replace(/\.jpg|\.png|\.jpeg/g, ''),
+      resource_type: 'image',
+    });
+    return result.url;
+  }
+
+  /**
+   * Upload video to cloudinary and return the url.
+   * @param {Express.Multer.File} video - The video to upload.
+   * @returns {Promise<string>} The url of the uploaded video.
+   */
+  private async uploadVideo(video: Express.Multer.File): Promise<string> {
+    const result = await this.cloundinaryService.uploadFile(video, {
+      public_id: video.originalname.replace(/\.mp4|\.mov/g, ''),
+      resource_type: 'video',
+    });
+    return result.url;
   }
 }
