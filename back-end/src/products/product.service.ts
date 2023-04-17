221ebd50873d8c42bdd461fa8ca31db18881dd82
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-17 21:00:16 +0000

    In the product service we added the logic to upload the product's images to cloudinary.

--- back-end/src/products/product.service.ts
+++ back-end/src/products/product.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from 'src/users/admin/dto/create-product.dto';
+import { CloudinaryService } from 'nestjs-cloudinary';
 
 /**
  * Service for product related operations.
@@ -13,6 +14,7 @@ export class ProductService {
   constructor(
     @InjectRepository(ProductEntity)
     private readonly typeormRepository: Repository<ProductEntity>,
+    private readonly cloudinaryService: CloudinaryService,
   ) {}
 
   /**
@@ -20,12 +22,38 @@ export class ProductService {
    * @param {CreateProductDto} product - The product to create.
    * @returns {Promise<ProductEntity>} The created product.
    */
-  async createProduct(product: CreateProductDto): Promise<ProductEntity> {
+  async createProduct(
+    product: CreateProductDto,
+    images: Express.Multer.File[],
+  ): Promise<ProductEntity> {
     try {
       const productEntity = this.typeormRepository.create(product);
+      productEntity.imagesURL = await this.uploadImages(images);
       return await this.typeormRepository.save(productEntity);
     } catch (error) {
       throw new InternalServerErrorException('Error creating product');
     }
+  }
+
+  /**
+   * Upload images to cloudinary and return the images URLs.
+   * @param {Express.Multer.File[]} images - The images to upload.
+   * @returns {Promise<string[]>} The images URLs.
+   */
+  private async uploadImages(images: Express.Multer.File[]): Promise<string[]> {
+    const imagesURL: string[] = [];
+
+    for (const image of images) {
+      const result = await this.cloudinaryService.uploadFile(image, {
+        /**
+         * Here we remove the file extension from the file original name
+         * because it will be added automatically by cloudinary.
+         */
+        public_id: image.originalname.replace(/\.jpg|\.png|\.jpeg/g, ''),
+      });
+      imagesURL.push(result.url);
+    }
+
+    return imagesURL;
   }
 }
