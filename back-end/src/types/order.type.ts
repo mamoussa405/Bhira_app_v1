8fbba1d6687db345e817dd91e0fdf66247e2c324
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 18:49:21 +0000

    In the order type we added interfaces that will be the stracture of the objects that will be returned to the user.

---
+++ back-end/src/types/order.type.ts
@@ -0,0 +1,49 @@
+/**
+ * This interface is used to define the structure of the order object
+ * that will be displayed in the cart page.
+ */
+export interface ICartOrder {
+  id: number;
+  quantity: number;
+  totalPrice: number;
+  product: {
+    name: string;
+    imageURL: string;
+  };
+}
+
+/**
+ * This interface is used to define the structure of the order object
+ * that will be displayed in the user profile page.
+ */
+export interface IUserProfileOrder {
+  id: number;
+  quantity: number;
+  totalPrice: number;
+  shipmentAddress: string;
+  orderTime: Date;
+  confirmedByAdmin: boolean;
+  product: {
+    name: string;
+    imageURL: string;
+  };
+}
+
+/**
+ * This interface is used to define the structure of the order object
+ * that will be displayed in the admin profile page.
+ */
+export interface IAdminProfileOrder {
+  id: number;
+  quantity: number;
+  totalPrice: number;
+  buyerName: string;
+  buyerPhoneNumber: string;
+  shipmentAddress: string;
+  orderTime: Date;
+  confirmedByAdmin: boolean;
+  product: {
+    name: string;
+    imageURL: string;
+  };
+}
