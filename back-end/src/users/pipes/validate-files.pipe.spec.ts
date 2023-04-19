9dd59ac89fed33da532952e4588431b477dbb415
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 22:33:23 +0000

    In the users folder we added the pipes folder to store the validation pipes, and we added to it the validate-files pipe that will validate the files uploaded like images and videos.

---
+++ back-end/src/users/pipes/validate-files.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ValidateFilesPipe } from './validate-files.pipe';
+
+describe('ValidateFilesPipe', () => {
+  it('should be defined', () => {
+    expect(new ValidateFilesPipe()).toBeDefined();
+  });
+});
