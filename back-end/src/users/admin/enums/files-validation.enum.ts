a40f71f4044d746d060e652125fffa410552bd95
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 23:18:30 +0000

    Moved the pipes and the enums folders to the parent of the admin folder

--- back-end/src/users/admin/enums/files-validation.enum.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This enum is used to let the validation pipe knows
- * which files to validate, in this case the product images
- * or the story files.
- */
-export enum FilesValidation {
-  PRODUCT = 'product',
-  STORY = 'story',
-}
--- back-end/src/users/admin/pipes/validate-files.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ValidateFilesPipe } from './validate-files.pipe';
-
-describe('ValidateFilesPipe', () => {
-  it('should be defined', () => {
-    expect(new ValidateFilesPipe()).toBeDefined();
-  });
-});
