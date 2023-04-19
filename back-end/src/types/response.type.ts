78650571740a42b829538006ca1b5f0e3a348a0d
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 18:50:39 +0000

    In the response type we added the interface that will be the stracture of the confirmation object that will be returned to the user when it needs a confirmation message.

---
+++ back-end/src/types/response.type.ts
@@ -0,0 +1,7 @@
+/**
+ * This interface is used to define the structure of the response object
+ * that will be sent to the client when a confirmation message is needed.
+ */
+export interface IConfirmationMessage {
+  message: string;
+}
