58551e34a0d434512a91fddfcb0b206943d90220
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 23:13:20 +0000

    In the users folder we added the types folder to store the custom types and the interfaces that we will need in the users folder, and we added to it the IProfile interface which represents the user and the admin profile.

---
+++ back-end/src/users/types/profile.type.ts
@@ -0,0 +1,13 @@
+import { IAdminProfileOrder } from 'src/types/order.type';
+import { IUserProfileOrder } from 'src/types/order.type';
+
+/**
+ * This interface represents the profile of a user or the admin.
+ */
+export interface IProfile {
+  name: string;
+  phoneNumber: string;
+  address: string;
+  avatarURL: string;
+  orders: IUserProfileOrder[] | IAdminProfileOrder[];
+}
