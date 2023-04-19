75b51a27dac56a3e2ae1a182c673e4251fe2d8d0
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 18:56:50 +0000

    Imported the ProfileModule.

--- back-end/src/users/admin/admin.module.ts
+++ back-end/src/users/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { AdminService } from './admin.service';
 import { ProductModule } from 'src/products/product.module';
 import { StoryModule } from 'src/stories/story.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { ProfileModule } from '../profile/profile.module';
 
 @Module({
-  imports: [ProductModule, StoryModule, MulterModule.register()],
+  imports: [ProductModule, StoryModule, ProfileModule, MulterModule.register()],
   controllers: [AdminController],
   providers: [AdminService],
 })
