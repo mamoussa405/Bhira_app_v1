28cbd3832e3c55279da4d4b33a43ccc2c4b764c6
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 15:32:54 +0000

    In the order controller we added the following routes handlers: * /orders/create:   A Post route that will call the createOrder method from the order service   to create a new order. * /orders/get:  A Get route that will call the getAddedToCartOrders method from the order service  to get all the orders that are still in the Cart of the current user. * /orders/delete/:id  A Delete route that will call the deleteOrder method from the order service  to delete a specific order.

--- back-end/src/orders/order.controller.ts
+++ back-end/src/orders/order.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
+import { OrderService } from './order.service';
+import {
+  CreateOrderDto,
+  CreateOrderParamDto,
+  CreateOrderQueryDto,
+} from './dto/order.dto';
+import { Request } from 'express';
+import { ICartOrder } from 'src/types/order.type';
 
-@Controller('order')
-export class OrderController {}
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post('create')
+  async createOrder(
+    @Body() order: CreateOrderDto,
+    @Query() queries: CreateOrderQueryDto,
+    @Req() req: Request,
+  ) {
+    return await this.orderService.createOrder(
+      order,
+      queries.productId,
+      req['user'].sub,
+    );
+  }
+
+  @Get('get')
+  async getAddedToCartOrders(@Req() req: Request): Promise<ICartOrder[]> {
+    return await this.orderService.getAddedToCartOrders(req['user'].sub);
+  }
+
+  @Delete('delete/:id')
+  async deleteOrder(@Param() params: CreateOrderParamDto, @Req() req: Request) {
+    return await this.orderService.deleteOrder(params.id, req['user'].sub);
+  }
+}
