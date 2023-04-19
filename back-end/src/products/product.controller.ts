ae6c9578432d8017c44eed6fdb0afaf81f548b88
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 18:23:06 +0000

    In the product controller we added the following routes handlers: * /products/get: this route is responsable for getting all products. * /products/get/:id: this route is responsable for getting one product. * /products/delete/:id: this route is responsable for deleting one product.

--- back-end/src/products/product.controller.ts
+++ back-end/src/products/product.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Query } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductParamDto, ProductQueryDto } from './dto/product.dto';
+import { INormalProduct } from './types/product.type';
 
-@Controller('product')
-export class ProductController {}
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get('get')
+  async getProducts(
+    @Query() queries: ProductQueryDto,
+  ): Promise<INormalProduct[]> {
+    return await this.productService.getProducts(queries.category);
+  }
+
+  @Get('get/:id')
+  async getProduct(@Param() params: ProductParamDto): Promise<INormalProduct> {
+    const product = await this.productService.getProduct(params.id);
+
+    return {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      imagesURL: product.imagesURL,
+    };
+  }
+
+  @Delete('delete/:id')
+  async deleteProduct(@Param() params: ProductParamDto) {
+    return await this.productService.deleteProduct(params.id);
+  }
+}
