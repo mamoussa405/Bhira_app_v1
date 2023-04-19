49d2d222c76a1795c40d96de435e9e4da3ef21f6
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-19 15:41:28 +0000

    Registred the TypeOrm Module, the product Module, and exported the order service to be used outside the order module.

--- back-end/src/orders/order.module.ts
+++ back-end/src/orders/order.module.ts
@@ -3,10 +3,17 @@ import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
+import { UserEntity } from 'src/users/auth/entities/user.entity';
+import { ProductModule } from 'src/products/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity])],
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity]),
+    TypeOrmModule.forFeature([UserEntity]),
+    ProductModule,
+  ],
   controllers: [OrderController],
   providers: [OrderService],
+  exports: [OrderService],
 })
 export class OrderModule {}
