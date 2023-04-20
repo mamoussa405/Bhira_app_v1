7fe72ec43b7ddf7a483679b20d59a9d560e8defe
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-20 17:52:46 +0000

    In the order controller we added the following route handler: * /orders/confirm: this route is responsable for confirming that the user wants to buy the orders that are in the cart.

--- back-end/src/orders/order.controller.ts
+++ back-end/src/orders/order.controller.ts
@@ -4,22 +4,32 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Query,
   Req,
+  UsePipes,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import {
+  ConfirmOrdersBuyDto,
   CreateOrderDto,
   CreateOrderParamDto,
   CreateOrderQueryDto,
 } from './dto/order.dto';
 import { Request } from 'express';
 import { ICartOrder } from 'src/types/order.type';
+import { TransformPhoneNumberPipe } from 'src/pipes/transform-phone-number.pipe';
+import { IConfirmationMessage } from 'src/types/response.type';
 
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Get('get')
+  async getAddedToCartOrders(@Req() req: Request): Promise<ICartOrder[]> {
+    return await this.orderService.getAddedToCartOrders(req['user'].sub);
+  }
 
   @Post('create')
   async createOrder(
@@ -34,9 +44,13 @@ export class OrderController {
     );
   }
 
-  @Get('get')
-  async getAddedToCartOrders(@Req() req: Request): Promise<ICartOrder[]> {
-    return await this.orderService.getAddedToCartOrders(req['user'].sub);
+  @Patch('confirm')
+  @UsePipes(TransformPhoneNumberPipe)
+  async confirmOrdersBuy(
+    @Body() body: ConfirmOrdersBuyDto,
+    @Req() req: Request,
+  ): Promise<IConfirmationMessage> {
+    return await this.orderService.confirmOrdersBuy(body, req['user'].sub);
   }
 
   @Delete('delete/:id')
