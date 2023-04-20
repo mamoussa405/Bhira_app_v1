40b85ad722f5db8e1132d681253ef4bdcde350ac
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-20 21:27:29 +0000

    We added the following route handler: * /user/profile/update/password: this route handler is responsable for updating the user password.

--- back-end/src/users/profile/profile.controller.ts
+++ back-end/src/users/profile/profile.controller.ts
@@ -14,22 +14,28 @@ import { TransformPhoneNumberPipe } from 'src/pipes/transform-phone-number.pipe'
 import {
   UpdateAddressDto,
   UpdateNameDto,
+  UpdatePasswordDto,
   UpdatePhoneNumberDto,
 } from './dto/profile.dto';
 import { ValidateFilesPipe } from '../pipes/validate-files.pipe';
 import { FilesValidation } from '../enums/files-validation.enum';
+import { IConfirmationMessage } from 'src/types/response.type';
+import { IProfile } from '../types/profile.type';
 
 @Controller('user/profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Get('get')
-  async getProfile(@Req() req: Request) {
+  async getProfile(@Req() req: Request): Promise<IProfile> {
     return await this.profileService.getProfile(req['user'].sub);
   }
 
   @Patch('update/name')
-  async updateName(@Req() req: Request, @Body() body: UpdateNameDto) {
+  async updateName(
+    @Req() req: Request,
+    @Body() body: UpdateNameDto,
+  ): Promise<IConfirmationMessage> {
     return await this.profileService.updateName(req['user'].sub, body.name);
   }
 
@@ -39,7 +45,7 @@ export class ProfileController {
     @Req() req: Request,
     @UploadedFiles(new ValidateFilesPipe(FilesValidation.PROFILE))
     avatar: Express.Multer.File[],
-  ) {
+  ): Promise<IConfirmationMessage> {
     return await this.profileService.updateAvatar(req['user'].sub, avatar[0]);
   }
 
@@ -48,7 +54,7 @@ export class ProfileController {
   async updatePhoneNumber(
     @Req() req: Request,
     @Body() body: UpdatePhoneNumberDto,
-  ) {
+  ): Promise<IConfirmationMessage> {
     return await this.profileService.updatePhoneNumber(
       req['user'].sub,
       body.phoneNumber,
@@ -56,10 +62,21 @@ export class ProfileController {
   }
 
   @Patch('update/address')
-  async updateAddress(@Req() req: Request, @Body() body: UpdateAddressDto) {
+  async updateAddress(
+    @Req() req: Request,
+    @Body() body: UpdateAddressDto,
+  ): Promise<IConfirmationMessage> {
     return await this.profileService.updateAddress(
       req['user'].sub,
       body.address,
     );
+  }
+
+  @Patch('update/password')
+  async updatePassword(
+    @Req() req: Request,
+    @Body() body: UpdatePasswordDto,
+  ): Promise<IConfirmationMessage> {
+    return await this.profileService.updatePassword(req['user'].sub, body);
   }
 }
