c55fa86457332c085766a83bc1fca59d2d7bcbd9
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-20 18:16:44 +0000

    We added the client type which will be responsable for storing the types and the interfaces that we will use in the client business logic.

---
+++ back-end/src/users/types/client.type.ts
@@ -0,0 +1,10 @@
+/**
+ * This interface is used to define the structure of the client object
+ * that we will return when the use requests the list of clients.
+ */
+export interface IClient {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  avatarURL: string;
+}
