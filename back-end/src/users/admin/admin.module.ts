c9e9743e13510c8b8a350e18f2476e4e04bf44aa
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-20 00:48:22 +0000

    Registred the user entity in the TypeOrmModule because it will be used by the AdminGuard.

--- back-end/src/users/admin/admin.module.ts
+++ back-end/src/users/admin/admin.module.ts
@@ -5,9 +5,19 @@ import { ProductModule } from 'src/products/product.module';
 import { StoryModule } from 'src/stories/story.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { ProfileModule } from '../profile/profile.module';
+import { OrderModule } from 'src/orders/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [ProductModule, StoryModule, ProfileModule, MulterModule.register()],
+  imports: [
+    ProductModule,
+    StoryModule,
+    ProfileModule,
+    OrderModule,
+    MulterModule.register(),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
