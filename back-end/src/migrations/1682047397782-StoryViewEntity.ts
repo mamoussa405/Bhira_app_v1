7570943359a46d3d0f0b914e36620cc06b460c70
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-26 22:14:56 +0000

    In the migrations folder we added a new migration for the storyViewEntity which will be recording which user views which story at which time.

---
+++ back-end/src/migrations/1682047397782-StoryViewEntity.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class StoryViewEntity1682047397782 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'story_view_entity',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'userId',
+            type: 'int',
+          },
+          {
+            name: 'storyId',
+            type: 'int',
+          },
+          {
+            name: 'viewedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['userId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'user_entity',
+            onDelete: 'CASCADE',
+          },
+          {
+            columnNames: ['storyId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'story_entity',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('story_view_entity');
+  }
+}
