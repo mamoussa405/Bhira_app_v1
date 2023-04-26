42056b48f362ae1b312a23e458f5207cf1a9cb9b
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-26 23:15:00 +0000

    Added the interface that represents the sturcture of the object to return when the client requests the current top market product.

--- back-end/src/products/types/product.type.ts
+++ back-end/src/products/types/product.type.ts
@@ -5,3 +5,13 @@ export interface INormalProduct {
   price: number;
   imagesURL: string[] | string;
 }
+
+export interface ITopMarketProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  imageURL: string;
+}
+// TODO: Limit the number of images uploaded to 1 if the product is a top market product.
