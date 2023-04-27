1f0b72418b8166aa11a92acee2d33d6d2084de76
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-27 00:00:46 +0000

    Registred the story view entity and the user entity for local uses.

--- back-end/src/stories/story.module.ts
+++ back-end/src/stories/story.module.ts
@@ -3,9 +3,13 @@ import { StoryController } from './story.controller';
 import { StoryService } from './story.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoryEntity } from './entities/story.entity';
+import { UserEntity } from 'src/users/auth/entities/user.entity';
+import { StoryViewEntity } from 'src/home/entities/story-view.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StoryEntity])],
+  imports: [
+    TypeOrmModule.forFeature([StoryEntity, UserEntity, StoryViewEntity]),
+  ],
   controllers: [StoryController],
   providers: [StoryService],
   exports: [StoryService],
