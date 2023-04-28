b60769c58d64464367d9369c38db910041f3d159
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-28 17:31:08 +0000

    We registred the AppGateway.

--- back-end/src/stories/story.module.ts
+++ back-end/src/stories/story.module.ts
@@ -5,13 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoryEntity } from './entities/story.entity';
 import { UserEntity } from 'src/users/auth/entities/user.entity';
 import { StoryViewEntity } from 'src/home/entities/story-view.entity';
+import { AppGateway } from 'src/app.gateway';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([StoryEntity, UserEntity, StoryViewEntity]),
   ],
   controllers: [StoryController],
-  providers: [StoryService],
+  providers: [StoryService, AppGateway],
   exports: [StoryService],
 })
 export class StoryModule {}
