e2e42ebfa2594112df14c8245b89b914a153157a
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-28 17:55:15 +0000

    Added some additional comments to the methods in the service.

--- back-end/src/users/auth/auth.service.ts
+++ back-end/src/users/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import {
+  HttpStatus,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -16,6 +17,9 @@ import { IConfirmationMessage } from 'src/types/response.type';
 
 /**
  * Service to handle user authentication, sign up and sign in.
+ * @function signUp sign up a new user
+ * @function signIn sign in a user
+ * @function signOut sign out a user
  */
 @Injectable()
 export class AuthService {
@@ -30,16 +34,24 @@ export class AuthService {
    *  Sign up a new user, hash password and save to database.
    * @param {SignUpDto} user user data
    * @returns {Promise<UserEntity>} user entity
+   * @throws {InternalServerErrorException} if could not create user
    */
   async signUp(user: SignUpDto): Promise<UserEntity> {
-    user.password = await this.hashPassword(user.password);
     try {
+      user.password = await this.hashPassword(user.password);
       const userEntity = this.userRepository.create(user);
+
       userEntity.avatarURL = this.configService.get<string>(
         'USER_DEFAULT_AVATAR',
       );
       return await this.userRepository.save(userEntity);
     } catch (error) {
+      /**
+       * If the error is due to duplicate phone number throw
+       * InternalServerErrorException with message.
+       * the contraint name should be changed if the database table name
+       * of the column name is changed.
+       */
       if (error.constraint === 'user_entity_phoneNumber_key')
         throw new InternalServerErrorException('Phone number already exists');
       throw new InternalServerErrorException('Could not create user');
@@ -47,44 +59,56 @@ export class AuthService {
   }
 
   /**
-   *  Sign in a user, generate jwt access token and set cookie.
+   * Sign in a user, generate jwt access token and set cookie.
    * @param {SignInDto} param0 user data
    * @param {Response} res response object
    * @returns {Promise<UserEntity>} user entity
+   * @throws {NotFoundException} if user not found
+   * @throws {UnauthorizedException} if user not confirmed or wrong password
+   * @throws {InternalServerErrorException} if could not sign in
    */
   async signIn(
     { phoneNumber, password }: SignInDto,
     res: Response,
   ): Promise<UserEntity> {
-    // First lets find the user by phone number
-    const userEntity = await this.userRepository.findOne({
-      where: { phoneNumber },
-    });
+    try {
+      /* First lets find the user by phone number */
+      const userEntity = await this.userRepository.findOne({
+        where: { phoneNumber },
+      });
 
-    // If user not found throw NotFoundException
-    if (!userEntity) {
-      throw new NotFoundException('User not found');
+      /* If user not found throw NotFoundException */
+      if (!userEntity) {
+        throw new NotFoundException('User not found');
+      }
+      /* Check if the user is not confirmed by the admin */
+      if (!userEntity.confirmedByAdmin) {
+        throw new UnauthorizedException('User not confirmed');
+      }
+      /* Compare password with hashed password */
+      const isMatch = await bcrypt.compare(password, userEntity.password);
+
+      /* If password does not match throw UnauthorizedException */
+      if (!isMatch) {
+        throw new UnauthorizedException('Wrong password');
+      }
+      /**
+       * Set cookie with jwt access token with httpOnly flag to
+       * disable access from client side.
+       */
+      res.cookie(
+        'access_token',
+        await this.jwtAccessToken(userEntity.phoneNumber, userEntity.id),
+        { httpOnly: true },
+      );
+      return userEntity;
+    } catch (error) {
+      if (error.status === HttpStatus.NOT_FOUND)
+        throw new NotFoundException(error.message);
+      if (error.status === HttpStatus.UNAUTHORIZED)
+        throw new UnauthorizedException(error.message);
+      throw new InternalServerErrorException('Could not sign in user');
     }
-    // Check if the user is not confirmed by the admin
-    if (!userEntity.confirmedByAdmin) {
-      throw new UnauthorizedException('User not confirmed');
-    }
-    // Compare password with hashed password
-    const isMatch = await bcrypt.compare(password, userEntity.password);
-    // If password does not match throw UnauthorizedException
-    if (!isMatch) {
-      throw new UnauthorizedException('Wrong password');
-    }
-    /**
-     * Set cookie with jwt access token with httpOnly flag to
-     * disable access from client side.
-     */
-    res.cookie(
-      'access_token',
-      await this.jwtAccessToken(userEntity.phoneNumber, userEntity.id),
-      { httpOnly: true },
-    );
-    return userEntity;
   }
 
   /**
