75773d443b9875ac3b5dea62a9ef935db3f445da
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-28 21:42:37 +0000

    We added the max length validation for the strings.

--- back-end/src/users/profile/dto/profile.dto.ts
+++ back-end/src/users/profile/dto/profile.dto.ts
@@ -4,6 +4,7 @@ import {
   IsString,
   IsStrongPassword,
   Matches,
+  MaxLength,
 } from 'class-validator';
 
 export class UpdateNameDto {
@@ -11,28 +12,33 @@ export class UpdateNameDto {
   @IsNotEmpty()
   // This regular expression matches only English and Arabic letters
   @Matches(/^[a-zA-Z\s\u0600-\u06FF]+$/)
+  @MaxLength(255)
   name: string;
 }
 
 export class UpdatePhoneNumberDto {
   @IsPhoneNumber('MA')
   @IsNotEmpty()
+  @MaxLength(255)
   phoneNumber: string;
 }
 
 export class UpdateAddressDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   address: string;
 }
 
 export class UpdatePasswordDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   oldPassword: string;
 
   @IsString()
   @IsNotEmpty()
   @IsStrongPassword()
+  @MaxLength(255)
   newPassword: string;
 }
