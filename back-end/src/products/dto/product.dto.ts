50c7fc35906459594e55e6c51ea00a119296e440
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-28 19:21:25 +0000

    We added the ProductQueryDto to validate the like string in case of product search.

--- back-end/src/products/dto/product.dto.ts
+++ back-end/src/products/dto/product.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumberString, Matches } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsString, Matches } from 'class-validator';
 
 export class ProductParamDto {
   @IsNumberString()
@@ -6,4 +6,12 @@ export class ProductParamDto {
   // This regular expression matches only positive integers
   @Matches(/^[1-9][0-9]*$/)
   id: number;
+}
+
+export class ProductQueryDto {
+  @IsNotEmpty()
+  @IsString()
+  // This regular expression matches only Arabic letters
+  @Matches(/^[\u0600-\u06FF]+$/)
+  like: string;
 }
