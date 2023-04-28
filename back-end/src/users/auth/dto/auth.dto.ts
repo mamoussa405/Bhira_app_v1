1b53f8e3feae592f6b4f0f2a9ca4d6db0735a1f2
Mohamed AMOUSSAOUI <mamoussa@LAPTOP-4GAJKA9E.localdomain>
2023-04-28 21:41:42 +0000

    We added the max length validation for the strings.

--- back-end/src/users/auth/dto/auth.dto.ts
+++ back-end/src/users/auth/dto/auth.dto.ts
@@ -4,6 +4,7 @@ import {
   IsString,
   IsStrongPassword,
   Matches,
+  MaxLength,
 } from 'class-validator';
 
 export class SignUpDto {
@@ -11,28 +12,34 @@ export class SignUpDto {
   @IsNotEmpty()
   // This regular expression matches only English and Arabic letters
   @Matches(/^[a-zA-Z\s\u0600-\u06FF]+$/)
+  @MaxLength(255)
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   address: string;
 
   @IsPhoneNumber('MA')
   @IsNotEmpty()
+  @MaxLength(255)
   phoneNumber: string;
 
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
+  @MaxLength(255)
   password: string;
 }
 
 export class SignInDto {
   @IsPhoneNumber('MA')
   @IsNotEmpty()
+  @MaxLength(255)
   phoneNumber: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   password: string;
 }
