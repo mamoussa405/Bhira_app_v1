610914f07064600e7cde0703671ad6bed5bc0119
Mohamed Amoussaoui <mamoussa@e1r7p1.1337.ma>
2023-05-11 18:53:16 +0100

    Added the required logic for the ws guard to verify the token when the user sends and event.

--- back-end/src/gateway/ws.guard.ts
+++ back-end/src/gateway/ws.guard.ts
@@ -1,10 +1,34 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
 
 @Injectable()
 export class WsGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToWs().getClient();
-    console.log(request);
+    const accessToken = request.handshake.headers.cookie
+      .split(': ')
+      .find((cookie: string) => cookie.startsWith('access_token'))
+      .split('=')[1];
+
+    if (!accessToken) throw new WsException('غير مصرح لك بالدخول');
+    try {
+      /**
+       * If there is an access_token, we verify it and set the user
+       * to the request object for future use.
+       */
+      await this.jwtService.verifyAsync(accessToken, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+    } catch (error) {
+      throw new WsException('غير مصرح لك بالدخول');
+    }
     return true;
   }
 }
